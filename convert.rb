6d641cc72698af83b208d162f7f58eb0035de3b9
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2015-11-14 01:42:43 -0600

    Removes slug as is generated by hugo, and creates the files

--- convert.rb
+++ convert.rb
@@ -10,22 +10,23 @@ db_username='root'
 db_password='1'
 db_database='diegomichel_org'
 db_posts_table='wp_m3yqzy_posts'
-destination='old_blog'
+destination='old_blog_posts/' #make sure includes the slash at the end.
 FileUtils::mkdir_p destination
 client = Mysql2::Client.new(host: db_host, username: db_username, password: db_password, database: db_database)
 rs = client.query "select post_date, post_status, post_title, post_content from #{db_posts_table}"
+count = 0
 rs.each(as: :array) do |row|
-  row[0] = row[0]
-  p row[0].inspect
   next if row[1] == 'inherit'
+  count += 1
   post_content = ReverseMarkdown.convert row[3].encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+  slug = row[2].gsub(/[^0-9a-z ]/i, '').tr(' ', '-').downcase
   post =<<-END.gsub(/^ {6}/, '')
-        +++
-        date = "#{row[0]}"
-  #{'draft = true' if row[1] == 'draft'}
-        title = "#{row[2]}"
-        slug = "#{row[2].gsub(/[^0-9a-z ]/i, '').tr(' ', '-').downcase}"
-        +++
-  #{post_content}
+      +++
+      date = "#{row[0]}"
+      #{'draft = true' if row[1] == 'draft'}
+      title = "#{row[2]}"
+      +++
+      #{post_content}
   END
+  File.open("#{destination}#{count}-#{slug}.md", 'w') {|f| f.write(post) }
 end
