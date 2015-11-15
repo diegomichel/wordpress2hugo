e619d385fc97240bb6a943741c5d76f494e8cac5
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2015-11-15 14:54:44 -0600

    Improves syntax

--- convert.rb
+++ convert.rb
@@ -1,31 +1,38 @@
 # encoding: UTF-8
 #!/usr/bin/ruby
-require 'pry'
 require 'reverse_markdown'
 require 'mysql2'
 require 'fileutils'
-# remember to execute first gem install mysql
-db_host='localhost'
-db_username='root'
-db_password='1'
-db_database='diegomichel_org'
-db_posts_table='wp_m3yqzy_posts'
-destination='old_blog_posts/' #make sure includes the slash at the end.
-FileUtils::mkdir_p destination
-client = Mysql2::Client.new(host: db_host, username: db_username, password: db_password, database: db_database)
+#####Configuration#####
+db_host = 'localhost'
+db_username = 'root'
+db_password = '1'
+db_database = 'diegomichel_org'
+db_posts_table = 'wp_m3yqzy_posts'
+destination = 'old_blog_posts/' # make sure includes the slash at the end.
+######################
+FileUtils.mkdir_p destination
+client = Mysql2::Client.new(host: db_host,
+                            username: db_username,
+                            password: db_password,
+                            database: db_database)
 rs = client.query "select post_date, post_status, post_title, post_content from #{db_posts_table}"
 count = 0
 rs.each(as: :array) do |row|
   next if row[1] == 'inherit'
   count += 1
-  post_content = ReverseMarkdown.convert row[3].encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+  post_content = ReverseMarkdown.convert row[3].encode('UTF-8',
+                                                       'binary',
+                                                       invalid: :replace,
+                                                       undef: :replace,
+                                                       replace: '')
   slug = row[2].gsub(/[^0-9a-z ]/i, '').tr(' ', '-').downcase
-  post =<<-END.gsub(/^ {6}/, '')
+  post = <<-END.gsub(/^ {6}/, '')
       +++
       date = "#{row[0]}"#{"\n      draft = true" if row[1] == 'draft'}
       title = "#{row[2]}"
       +++
       #{post_content}
   END
-  File.open("#{destination}#{count}-#{slug}.md", 'w') {|f| f.write(post) }
+  File.open("#{destination}#{count}-#{slug}.md", 'w') { |f| f.write(post) }
 end
